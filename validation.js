57cd7cdf3fcd6f55e1d93041e061d8bc546af83b
Asim Amin <altairasim@gmail.com>
2022-11-10 05:42:26 -0800

    added messaging system

--- validation.js
+++ validation.js
@@ -1,28 +1,21 @@
 var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 var inputText = document.getElementById('email-field');
 var crossIcon = document.getElementById('cross-icon');
-var checkIcon = document.getElementById('check-icon');
 const sendButton =  document.getElementById('send-button'); 
 
 function validate(){
     if(inputText.value.match(mailformat)){
-        checkIcon.classList.remove('hide');
-        crossIcon.classList.add('hide');
-        sendButton.removeAttribute('disabled', '')
+        sendButton.removeAttribute('disabled', ''); 
         sendButton.style.cursor = 'pointer';
+        crossIcon.classList.add('hide');
     } else{
         crossIcon.classList.remove('hide');
-        checkIcon.classList.add('hide');
-        sendButton.setAttribute('disabled', '')
+        sendButton.setAttribute('disabled', '');
         sendButton.style.cursor = 'not-allowed';
     }
 }
 
 
-
-
-
-
-
-
-
+function submited(){
+    alert("Message sent successfully");
+}
